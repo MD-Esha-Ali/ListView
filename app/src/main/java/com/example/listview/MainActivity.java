a7c26735c8b164072e99f643531ed8a6ae32ca9b
MD ESHA ALI <eshaalinpi@gmail.com>
2020-02-14 16:20:02 -0800

    List View

---
+++ app/src/main/java/com/example/listview/MainActivity.java
@@ -0,0 +1,32 @@
+package com.example.listview;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    private ListView listView;
+    private String[] contryNames;
+    private int[] love={R.drawable.a, R.drawable.b, R.drawable.c, R.drawable.d, R.drawable.e, R.drawable.f, R.drawable.g, R.drawable.h, R.drawable.i, R.drawable.j, R.drawable.k, R.drawable.l, R.drawable.m, R.drawable.n};
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        listView=(ListView) findViewById(R.id.ListViewId);
+        contryNames=getResources().getStringArray(R.array.contry_names);
+        ArrayAdapter<String>  arrayAdapter=new ArrayAdapter<String>(MainActivity.this,R.layout.symple_example,R.id.TextViewId,contryNames);
+        listView.setAdapter(arrayAdapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String value=contryNames[position];
+                Toast.makeText(MainActivity.this,value,Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
